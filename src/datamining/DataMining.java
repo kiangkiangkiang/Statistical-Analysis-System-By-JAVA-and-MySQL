1e2684b4e38e3e5c1986815f99a9f6ac05333922
kiangkiangkiang <44896160+kiangkiangkiang@users.noreply.github.com>
2021-12-28 23:17:48 +0800

    Add files via upload

---
+++ src/datamining/DataMining.java
@@ -0,0 +1,14 @@
+
+package datamining;
+
+public class DataMining {
+
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DM().setVisible(true);
+            }
+        });
+    }
+    
+}
